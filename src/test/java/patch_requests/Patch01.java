524b2136fe130f6e8dea8a5925f8311afcb1d1a7
Naif aslan <naifasln3121@gmail.com>
2022-11-04 23:33:24 +0300

    naif

--- src/test/java/patch_requests/Patch01.java
+++ src/test/java/patch_requests/Patch01.java
@@ -1,4 +1,6 @@
-package put_requests;
+package patch_requests;
+
+
 
 import base_urls.JsonplaceholderBaseUrl;
 import io.restassured.http.ContentType;
@@ -12,48 +14,46 @@ import java.util.Map;
 import static io.restassured.RestAssured.given;
 import static org.junit.Assert.assertEquals;
 
-public class Put01 extends JsonplaceholderBaseUrl {
-    /*
+public class Patch01 extends JsonplaceholderBaseUrl {
+     /*
         Given
            1) https://jsonplaceholder.typicode.com/todos/198
            2) {
-                 "userId": 21,
-                 "title": "Wash the dishes",
-                 "completed": false
+                 "title": "Wash the dishes"
                }
         When
-         I send PUT Request to the Url
+         I send PATCH Request to the Url
        Then
              Status code is 200
              And response body is like   {
-                               "userId": 21,
+                               "userId": 10,
                                "title": "Wash the dishes",
-                               "completed": false
+                               "completed": true,
                                "id": 198
-                              }
+                               }
      */
 
     @Test
-    public void put01(){
+    public void patch01(){
+
         //Set the Url
-        spec.pathParams("first", "todos","second",198);
+        spec.pathParams("first","todos","second",198);
 
         //Set the Expected Data
         JsonPlaceHolderTestData obj = new JsonPlaceHolderTestData();
-        Map<String , Object> expectedData = obj.expectedDataMethod(21,"Wash the dishes",false);
+        Map<String ,Object> expectedData = obj.expectedDataMethod(null, "Wash the dishes", null);
         System.out.println("expectedData = " + expectedData);
 
         //Send the Request and Get the Response
-        Response response = given().spec(spec).contentType(ContentType.JSON).body(expectedData).when().put("/{first}/{second}");
+        Response response = given().spec(spec).contentType(ContentType.JSON).body(expectedData).when().patch("/{first}/{second}");
         response.prettyPrint();
 
         //Do Assertion
-        Map<String , Object> actualData =  response.as(HashMap.class);
+        Map<String,Object> actualData = response.as(HashMap.class);
         System.out.println("actualData = " + actualData);
 
-        assertEquals(expectedData.get("completed"),actualData.get("completed"));
+        assertEquals(200,response.statusCode());
         assertEquals(expectedData.get("title"),actualData.get("title"));
-        assertEquals(expectedData.get("userId"),actualData.get("userId"));
 
     }
 }
