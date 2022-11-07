365ae631bc0fc516f1bc79d1e0520d150cf567e3
Naif aslan <naifasln3121@gmail.com>
2022-11-07 23:22:34 +0300

    naif

---
+++ src/test/java/post_requests/Post06.java
@@ -0,0 +1,46 @@
+package post_requests;
+
+import base_urls.DummyRestApiBaseUrl;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class Post06 extends DummyRestApiBaseUrl {
+
+    /*
+       URL: https://dummy.restapiexample.com/api/v1/create
+       HTTP Request Method: POST Request
+       Request body:
+                     {
+                        "employee_name": "Tom Hanks",
+                        "employee_salary": 111111,
+                        "employee_age": 23,
+                        "profile_image": "Perfect image",
+                        "id": 4891
+                     }
+       Test Case: Type by using Gherkin Language
+       Assert:
+                i) Status code is 200
+                ii) Response body should be like the following
+                    {
+                        "status": "success",
+                        "data": {
+                            "employee_name": "Tom Hanks",
+                            "employee_salary": 111111,
+                            "employee_age": 23,
+                            "profile_image": "Perfect image",
+                            "id": 4891
+                        },
+                        "message": "Successfully! Record has been added."
+                    }
+     */
+
+    @Test
+    public void post06(){
+
+        spec.pathParam("first","create");
+
+
+    }
+
+}
