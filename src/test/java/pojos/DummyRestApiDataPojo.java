61b8626938c2a939d2cbd83c2c5d6cf91f97b850
Naif aslan <naifasln3121@gmail.com>
2022-11-08 00:21:30 +0300

    naif

--- src/test/java/pojos/DummyRestApiDataPojo.java
+++ src/test/java/pojos/DummyRestApiDataPojo.java
@@ -1,17 +1,20 @@
 package pojos;
 
-public class DummyRestApiDataPojo {
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class DummyRestApiDataPojo {
     private String employee_name;
     private Integer employee_salary;
     private Integer employee_age;
     private String profile_image;
 
-    public DummyRestApiDataPojo(String employee_name, Integer employee_salary, Integer employee_age, String profile_image) {
-        this.employee_name = employee_name;
-        this.employee_salary = employee_salary;
-        this.employee_age = employee_age;
-        this.profile_image = profile_image;
+
+    public DummyRestApiDataPojo(String employeeName, Integer employeeSalary, Integer employeeAge, String profileImage) {
+        this.employee_name = employeeName;
+        this.employee_salary = employeeSalary;
+        this.employee_age = employeeAge;
+        this.profile_image = profileImage;
     }
 
     public DummyRestApiDataPojo() {
@@ -20,34 +23,28 @@ public class DummyRestApiDataPojo {
     public String getEmployee_name() {
         return employee_name;
     }
-
     public void setEmployee_name(String employee_name) {
         this.employee_name = employee_name;
     }
-
     public Integer getEmployee_salary() {
         return employee_salary;
     }
-
     public void setEmployee_salary(Integer employee_salary) {
         this.employee_salary = employee_salary;
     }
-
     public Integer getEmployee_age() {
         return employee_age;
     }
-
     public void setEmployee_age(Integer employee_age) {
         this.employee_age = employee_age;
     }
-
     public String getProfile_image() {
         return profile_image;
     }
-
     public void setProfile_image(String profile_image) {
         this.profile_image = profile_image;
     }
+
 
     @Override
     public String toString() {
